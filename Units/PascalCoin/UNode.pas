92b176b866dad3d26feb7731d7a798f4cbf60541
xiphon <xiphon@protonmail.com>
2017-03-03 13:37:19 +0000

    Fixed findoperation RPC request was unable to find existing transaction (#1, PascalCoin#23)

--- Units/PascalCoin/UNode.pas
+++ Units/PascalCoin/UNode.pas
@@ -658,6 +658,9 @@ begin
         end;
         If op.Previous_Sender_updated_block>block then exit;
         block := op.Previous_Sender_updated_block;
+      end else if (op.OpType = CT_Op_Transaction) and (TOpTransaction(op).Data.target = account) then begin
+        If op.Previous_Destination_updated_block > block then exit;
+        block := op.Previous_Destination_updated_block;
       end;
     end;
     if (block>=aux_block) then exit; // Error... not found a valid block positioning
